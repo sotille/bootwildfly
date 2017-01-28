8d7230c14a366262be3f4bffda342f3e3bb95503
sotille <felipe.sotille@wplex.com.br>
2017-01-28 10:05:30 -0200

    ss

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -4,19 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("/")
 public class HelloWildFlyController
 {
 
     @RequestMapping("hell")
     public String sayHello()
     {
-        return ("Hello, SpringBoot on Wildfly");
+        return ("Hello, SpringBoot on Wildfly asfsadfsafd ");
     }
 
     @RequestMapping("hell1")
     public String sayHello1()
     {
-        return ("Hello, SpringBoot on Wildfly");
+        return ("Hello, SpringBoot on Wildfly safdsadf sdf");
     }
 }
